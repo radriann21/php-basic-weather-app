78570525a86922c328849c06c184c1a440ff00ec
radriann21 <adrianrangelargu@gmail.com>
2024-06-21 16:01:44 -0400

    first commit - setup project

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Weather PHP App</title>
+  <link rel="stylesheet" href="./output.css">
+</head>
+<body>
+  
+  <main class="max-w-full min-h-screen flex items-center justify-center">
+    <article class="w-[40%] h-[430px] bg-red-700">
+      
+    </article>
+  </main>
+
+</body>
+</html>
